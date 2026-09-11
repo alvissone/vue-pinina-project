e894693e92ac3b6da9ad265e0309e1954b03a5b8
alvissone8 <alvissone8@gmail.com>
2026-09-11 23:11:35 +0800

    新增ui库element-plus

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,8 +1,37 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <el-timeline style="max-width: 600px">
+      <el-timeline-item
+        v-for="(activity, index) in activities"
+        :key="index"
+        :timestamp="activity.timestamp"
+        :type="activity.type"
+        :hollow="activity.hollow"
+      >
+        {{ activity.content }}
+      </el-timeline-item>
+    </el-timeline>
   </div>
 </template>
+
+<script lang="ts" setup>
+const activities = [
+  {
+    content: '唐诡之西行S02E12',
+    timestamp: '2024-11-13 00:49:00',
+  },
+  {
+    content: 'Approved',
+    timestamp: '2018-04-13',
+  },
+  {
+    content: 'Custom hollow',
+    timestamp: '2018-04-03 20:46',
+    type: 'primary',
+    hollow: true,
+  },
+]
+</script>
 
 <style>
 @media (min-width: 1024px) {
